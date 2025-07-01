fb27cd29d48a9506e2c12ef90972bf1d09be4001
Bruno Salvador <bruno.pereira@visie.com.br>
2025-07-01 20:05:02 -0300

    refactor: update background to run settings

--- background.js
+++ background.js
@@ -1,10 +1,27 @@
+// Values for settings
+let autoSaveReport = false
+let autoSaveInterval = 10
+let maxDaysReport = 7
+let intervalId = null
+
+
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.local.set({ tasks: {} })
 })
-chrome.runtime.onStartup.addListener(() => {
-  cleanOldReportEntries()
-})
 
+// Load settings from storage
+chrome.runtime.onStartup.addListener(() => {
+  cleanOldReportEntries();
+  chrome.storage.local.get(['autoSaveReport', 'autoSaveInterval', 'maxDaysReport'], data => {
+    autoSaveReport = typeof data.autoSaveReport === 'boolean' ? data.autoSaveReport : autoSaveReport;
+    autoSaveInterval = data.autoSaveInterval;
+    maxDaysReport = data.maxDaysReport;
+    if (autoSaveReport) {
+      if (intervalId) clearInterval(intervalId);
+      intervalId = setInterval(saveReportSnapshot, autoSaveInterval * 60 * 1000);
+    }
+  });
+})
 
 function saveReportSnapshot() {
   chrome.storage.local.get(['tasks', 'report'], data => {
@@ -30,21 +47,38 @@ function saveReportSnapshot() {
 }
 
 
-function isLast7Days(isoDate) {
+function isLastDays(isoDate) {
   const now = new Date()
   const created = new Date(isoDate)
   const diff = (now - created) / (1000 * 60 * 60 * 24)
-  return diff <= 7
+  return diff <= maxDaysReport
 }
 
 
 function cleanOldReportEntries() {
   chrome.storage.local.get(['report'], data => {
     if (!data.report) return
-    const filteredReport = data.report.filter(entry => isLast7Days(entry.date))
+    const filteredReport = data.report.filter(entry => isLastDays(entry.date))
     chrome.storage.local.set({ report: filteredReport })
   })
 }
 
-// Runs every 30 minutes
-setInterval(saveReportSnapshot, 30 * 60 * 1000)
+
+chrome.runtime.onMessage.addListener((message) => {
+  if (message.type === 'settings-updated') {
+    chrome.storage.local.get(['autoSaveInterval', 'maxDaysReport', 'autoSaveReport'], data => {
+      autoSaveInterval = data.autoSaveInterval || autoSaveInterval
+      maxDaysReport = data.maxDaysReport || maxDaysReport
+      autoSaveReport = data.autoSaveReport
+
+      // Update interval for auto-saving report
+      if (intervalId) clearInterval(intervalId)
+      if (autoSaveReport) {
+        intervalId = setInterval(saveReportSnapshot, autoSaveInterval * 60 * 1000)
+      } else {
+        clearInterval(intervalId)
+        intervalId = null
+      }
+    })
+  }
+})
